924adff02f0888b445b7de3dfcf98a9d297dee10
jdiec <jacques.diec@gmail.com>
2021-01-14 09:36:17 +0000

    Add trackable to devise user

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :courses
   root 'home#index'
   get 'home/index'
-  # get 'landing_page', to: 'static_pages#landing_page'
-  # get 'privacy_policy', to: 'static_pages#privacy_policy'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  resources :users, only: [:index]
+  resources :courses
 end
