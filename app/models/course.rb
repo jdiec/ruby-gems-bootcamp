4c728b0b799a1cabf6ebfad0be083cf5615249e5
jdiec <jacques.diec@gmail.com>
2021-01-15 20:41:34 +0000

    Add additional fields to courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,6 @@
 class Course < ApplicationRecord
   belongs_to :user
-  validates :title, presence: true
+  validates :title, :short_description, :language, :price, :level, presence: true
   validates :description, presence: true, length: { :minimum => 5 }
   has_rich_text :description
   
