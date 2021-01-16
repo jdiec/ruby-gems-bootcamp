57798cb24e26b0a3cc30b0128b202372611c84b1
jdiec <jacques.diec@gmail.com>
2021-01-16 02:06:55 +0000

    Add edit functionality to user roles

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'home/index'
   get 'home/activity'
   devise_for :users
-  resources :users, only: [:index]
+  resources :users, only: [:index, :edit, :show, :update]
   resources :courses
 end
